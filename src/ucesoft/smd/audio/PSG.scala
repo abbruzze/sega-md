69c0a833d6c104a87f93aa3372e078cc4de445cc
Alessandro Abbruzzetti <abbruzze@gmail.com>
2023-12-19 16:15:46 +0100

    Now is stereo 16 bit

--- src/ucesoft/smd/audio/PSG.scala
+++ src/ucesoft/smd/audio/PSG.scala
@@ -7,6 +7,8 @@ package ucesoft.smd.audio
 class PSG(sampleRate:Int, override val name:String) extends AudioDevice(sampleRate,name):
   final val sn76489 = new SN76489
 
+  override final def isStereo: Boolean = true
+
   override def reset(): Unit =
     super.reset()
     sn76489.reset()
@@ -15,4 +17,8 @@ class PSG(sampleRate:Int, override val name:String) extends AudioDevice(sampleRa
     super.setCPUFrequency(f)
     sn76489.init(f,sampleRate)
 
-  override final protected def getLevelMono8Bit(): Int = sn76489.clock()
+  //override final protected def getLevelMono8Bit(): Int = sn76489.clock()
+  override final protected def getLevelStereo16Bit(LR:Array[Int]): Unit =
+    val value16 = sn76489.clock() << 7
+    LR(0) = value16
+    LR(1) = value16
