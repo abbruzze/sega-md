ed09f768d9a3ec4c68ef1cbbbbcedd66a94b8d6c
Alessandro Abbruzzetti <abbruzze@gmail.com>
2024-01-28 20:16:34 +0100

    Finalized

--- src/ucesoft/smd/cheat/Cheat.scala
+++ src/ucesoft/smd/cheat/Cheat.scala
@@ -1,5 +1,10 @@
 package ucesoft.smd.cheat
 
+import org.yaml.snakeyaml.Yaml
+import ucesoft.smd.Logger
+
+import java.util.zip.GZIPInputStream
+
 /**
  * @author Alessandro Abbruzzetti
  *         Created on 06/01/2024 15:53
@@ -7,6 +12,11 @@ package ucesoft.smd.cheat
  * Credits to http://games.technoplaza.net/ggencoder/java/
  */
 object Cheat:
+  case class CheatEntry(cheats:List[CheatCode],description:String)
+  case class GameCheats(name:String,regions:List[String],cheats:Array[CheatEntry]):
+    override def toString: String =
+      s"GameCheats($name,$regions,${cheats.mkString("[",",","]")}"
+  case class Game(name:String,crc32:String,size:String,year:String)
   case class CheatCode(address:Int,value:Int):
     private var romOldValue = 0
     private var romPatched = false
@@ -41,6 +51,59 @@ object Cheat:
   )
   private val CODE_RE = "^([A-Za-z0-9]{4})-([A-Za-z0-9]{4})$".r
   private val RAW_CODE_RE = "^([A-Fa-f0-9]{1,6}):([A-Fa-f0-9]{1,4})$".r
+
+  final val gameCheats = loadGameCheats()
+  final val games = loadGames()
+
+  private def loadGames(): Map[String,Game] =
+    import java.util.{List as JList, Map as JMap}
+    import scala.jdk.CollectionConverters.*
+
+    val yaml = new Yaml()
+    val in = classOf[Cheat.type].getResourceAsStream("/resources/gamesDB.yaml.gz")
+    if in == null then
+      Logger.getLogger.error("Cannot load cheats file: /resources/gamesDB.yaml.gz")
+      println("Cannot load cheats file")
+      return Map.empty
+    val gin = new GZIPInputStream(in)
+    val db = yaml.load[JList[JMap[String, AnyRef]]](gin).asScala.toList
+    val games = for g <- db yield
+      val game = g.asScala
+      val name = game("game").toString
+      val crc32 = game("crc32").toString
+      val size = Option(game.getOrElse("size","unknown")).getOrElse("unknown").toString
+      val year = Option(game.getOrElse("year","unknown")).getOrElse("unknown").toString
+      crc32 -> Game(name,crc32,size,year)
+
+    gin.close()
+    games.toMap
+  private def loadGameCheats(): List[GameCheats] =
+    import java.util.{List as JList, Map as JMap}
+    import scala.jdk.CollectionConverters.*
+
+    val yaml = new Yaml()
+    val in = classOf[Cheat.type].getResourceAsStream("/resources/cheatsDB.yaml.gz")
+    if in == null then
+      Logger.getLogger.error("Cannot load cheats file: /resources/cheatsDB.yaml.gz")
+      println("Cannot load cheats file")
+      return Nil
+    val gin = new GZIPInputStream(in)
+    val db = yaml.load[JList[JMap[String, AnyRef]]](gin).asScala.toList
+    val games = for g <- db yield
+      val game = g.asScala
+      val name = game("game").toString
+      val regions = game("regions").asInstanceOf[JList[String]].asScala.toList
+      val codes = game("codes").asInstanceOf[JList[JMap[String,AnyRef]]].asScala.toList
+      val cheats = for code <- codes yield
+        val c = code.asScala.toMap
+        val desc = c("desc").toString
+        val ccc = c("code").asInstanceOf[JList[String]].asScala.toList
+        val cc = c("code").asInstanceOf[JList[String]].get(0).split(",").flatMap(decode).toList
+        CheatEntry(cc,desc)
+      GameCheats(name,regions,cheats.toArray)
+
+    gin.close()
+    games
 
   def decode(code:String): Option[CheatCode] =
     decodeGG(code).orElse(decodeRaw(code))
